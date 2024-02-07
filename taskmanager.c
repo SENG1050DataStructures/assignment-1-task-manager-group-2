0a96cfc0f2dfdb6cec4cfc4f3e6fc7cf39fffc1e
Joshua Rice <Jrice1041@conestogac.on.ca>
2024-02-06 19:16:57 -0500

    Added Struct definition, libraries & array sizes

--- taskmanager.c
+++ taskmanager.c
@@ -1,4 +1,19 @@
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+
+#define MAX_TITLE_SIZE 100
+#define MAX_DESCRIPTION_SIZE 100
+
+typedef struct Task {
+	int TaskId;
+	char Title[MAX_TITLE_SIZE];
+	char Description[MAX_DESCRIPTION_SIZE];
+	Task* NextTask;
+} Task;
+
+
 
 int main(void)
 {
